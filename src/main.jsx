faf5a7cc235dbd5712a3b95c8862e665d49a699d
andreama2021 <83208048+andreama2021@users.noreply.github.com>
2026-02-23 19:26:18 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import InvestorDashboard from './investor-dashboard.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import InvestorDashboard from './investor-dashboard'
+import InvestorPitchDashboard from './investor-pitch-dashboard'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InvestorDashboard />
+    <Router>
+      <Routes>
+        <Route path="/" element={<InvestorDashboard />} />
+        <Route path="/pitch" element={<InvestorPitchDashboard />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
